0fccf6a5419cc1cb8fed057312ee55caf17652d4
Pawelllaz <pawelllaz@gmail.com>
2018-11-21 00:22:25 +0100

    done

--- 5/polypoly/polypoly/poly.cpp
+++ 5/polypoly/polypoly/poly.cpp
@@ -3,26 +3,83 @@
 #include <math.h>
 #include <map>
 #include "poly.h"
-
+#include <sstream>
 using namespace std;
-
-double &poly::operator[](const int i)
-{
-	return this->p[i];
-}
-
 
 poly::poly(double new_number)
 {
 	this->p[0] = new_number;
 }
-
-ostream &operator<<(ostream &out, poly po)
+//================================================================
+double &poly::operator[](const int i)
 {
-    string s = "";
-    for(map<int,double>::const_iterator i = po.p.begin(); i!=po.p.end(); i++)
-    {
-        if((*i).second!=0) s += (*i).second.str();
+	return this->p[i];
+}
+//================================================================
+poly poly::operator+ (const poly &po) const
+{	
+	poly new_poly;
+	
+	for (std::map<int, double>::const_iterator i = p.begin(); i != p.end(); ++i)
+		new_poly.p[(*i).first] = (*i).second;
 
-    }
+	for (std::map<int, double>::const_iterator i = po.p.begin(); i != po.p.end(); ++i)
+	{
+		new_poly.p[(*i).first] += (*i).second;
+		if (new_poly.p[(*i).first] == 0) new_poly.p.erase((*i).first);
+	}
+	return new_poly;
+}
+//================================================================
+poly operator* (const poly &p1,const poly &p2) 
+{
+	poly new_poly;
+
+	for (std::map<int, double>::const_iterator i = p1.p.begin(); i != p1.p.end(); ++i)
+	{
+		for (std::map<int, double>::const_iterator j = p2.p.begin(); j != p2.p.end(); ++j)
+		{
+			int id = (*i).first + (*j).first;
+			new_poly.p[id] = (*i).second * (*j).second;
+			if (new_poly.p[id] == 0) new_poly.p.erase(id);
+		}
+	}
+	return new_poly;
+}
+//================================================================
+double poly::operator() (double x)
+{
+	double result = 0;
+	for (std::map<int, double>::const_iterator i = p.begin(); i != p.end(); ++i)
+		result += (*i).second * pow(x, (*i).first);
+	return result;
+}
+//================================================================
+ostream &operator<<(std::ostream &out, poly po)
+{
+	stringstream s;
+	if (po.p.begin() == po.p.end()) return out << "0";
+	int size = po.p.size();
+	for (std::map<int,double>::const_iterator i = --po.p.end(); i != po.p.begin() ; i--)
+	{
+		if ((*i).second != 0)
+		{
+			if ((*i).second != 1) s << (*i).second;
+			if ((*i).first > 1)
+			{
+				s << "x^";
+				s << (*i).first;
+			}
+			else s << "x";
+
+			if ((*i).first - 1 != 0) s << " + ";
+		}
+	}
+
+	if ((*po.p.begin()).second != 0)
+	{
+		s << " + ";
+		s << (*po.p.begin()).second;
+	}
+	return out << s.str();
 }
--- 5/polypoly/polypoly/poly.h
+++ 5/polypoly/polypoly/poly.h
@@ -1,7 +1,6 @@
 #ifndef __POLY_H__
 #define __POLY_H__
 #include <iostream>
-#include <math.h>
 #include <map>
 
 using namespace std;
@@ -9,15 +8,14 @@ using namespace std;
 class poly
 {
 private:
-	map<int, double> p;
+	std::map<int, double> p;
 public:
+	poly(double new_number = 0);
 	double &operator[] (const int i);
-    friend std::ostream &operator<<(std::ostream &out, poly & po);
-    /*{
-		po.p.begin(); // a tu widzi 
-    }*/
-
-	poly(double new_number=0);
+	poly operator+ (const poly &po) const;
+	double operator() (double x);
+	friend poly operator* (const poly &p1, const poly &p2);
+	friend std::ostream &operator<<(std::ostream &out, poly po);
 };
-// Complex m(s1); konstruktor kopiujący
+
 #endif /* __POLY_H__ */
